55ad2f999f9f6167ce67c46b53ca1f9c7499537f
narae <skfo0827@naver.com>
2025-10-14 19:19:33 +0900

    style(ProjectDetail): Header 조건부 렌더링

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,15 +1,23 @@
 // src/layouts/RootLayout.jsx
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-export default function RootLayout() {
+const RootLayout = () => {
+    const { pathname } = useLocation();
+    // /projects/:id 에서만 헤더 숨김
+    const hideHeader = /^\/projects\/[^/]+$/.test(pathname);
+
     return (
-        <>
-            <Header />
-            <Outlet />
+        <div id="wrap">
+            {!hideHeader && <Header />}
+            <main id="main">
+                <Outlet />
+            </main>
             <Footer />
-        </>
+        </div>
     );
-}
+};
+
+export default RootLayout;
