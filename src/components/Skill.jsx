dff282fa1144991409c42b6c130043a9fd19bc11
narae <skfo0827@naver.com>
2025-10-19 14:22:04 +0900

    feat(skill): 이력 섹션 데이터화(resumeData) 및 동적 렌더링

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,6 +1,6 @@
 // Skill.jsx
 import React from "react";
-import { skillText } from "../constants";
+import { Interview, resumeData } from "../constants";
 import SvgIcon from "./SvgIcon/SvgIcon";
 
 const Skill = () => {
@@ -10,9 +10,53 @@ const Skill = () => {
                 <h2 className="skill__title">ABOUT ME</h2>
 
                 <div className="skill__content">
+                    {/* 이력 */}
+                    <ul className="skill__desc">
+                        {/* Career */}
+                        <li className="skill__desc-item profile">
+                            <h3>Career</h3>
+                            <ul>
+                                {resumeData.career.map((c, i) => (
+                                <li key={`career-${i}`}>
+                                    <div className="skill__meta">
+                                    <p className="skill__date">{c.from} - {c.to}</p>
+                                    <span className="skill__divider" aria-hidden="true" />
+                                    <b>{c.company}</b>
+                                    </div>
+                                    <div className="skill__info">
+                                    <span>{c.role}</span>
+                                    {c.desc && <p>{c.desc}</p>}
+                                    </div>
+                                </li>
+                                ))}
+                            </ul>
+                        </li>
+
+                        {/* Education */}
+                        <li className="skill__desc-item profile">
+                            <h3>Education</h3>
+                            <ul>
+                                {resumeData.education.map((e, i) => (
+                                <li key={`edu-${i}`}>
+                                    <div className="skill__meta">
+                                    <p className="skill__date">{e.from} ~ {e.to}</p>
+                                    <span className="skill__divider" aria-hidden="true" />
+                                    <b>{e.school}</b>
+                                    </div>
+                                    <div className="skill__info">
+                                    <span>{e.course}</span>
+                                    {e.desc && <p>{e.desc}</p>}
+                                    </div>
+                                </li>
+                                ))}
+                            </ul>
+                        </li>
+                    </ul>
+
                     {/* 소개 리스트 */}
                     <ul className="skill__desc">
-                        {skillText.map((skill, key) => (
+                        <h3 className="skill__inner-title">Interview</h3>
+                        {Interview.map((skill, key) => (
                             <li key={key} className="skill__desc-item">
                                 <h3>
                                     <span>{key + 1}.</span>
@@ -25,13 +69,13 @@ const Skill = () => {
 
                     {/* 기술/툴 아이콘 */}
                     <div className="skill__tools">
-                        <h3>Skill & Tools</h3>
+                        <h3 className="skill__inner-title">Skill & Tools</h3>
                         <div className="skill__tools-group">
                             <h4>FrontEnd</h4>
                             <ul className="skill__icons">
                                 {["html", "css", "javascript", "react", "tailwind", "sass"].map((n) => (
                                     <li key={n} className="icon__cover">
-                                    <SvgIcon name={n} className={`icon__svg ${n}`} />
+                                        <SvgIcon name={n} className={`icon__svg ${n}`} />
                                     </li>
                                 ))}
                             </ul>
@@ -41,9 +85,9 @@ const Skill = () => {
                             <h4>Tools</h4>
                             <ul className="skill__icons">
                                 {["github", "figma", "xd", "illustrator", "photoshop", "slack", "notion", "discord"].map((n) => (
-                                <li key={n} className="icon__cover">
-                                    <SvgIcon name={n} className={`icon__svg ${n}`} />
-                                </li>
+                                    <li key={n} className="icon__cover">
+                                        <SvgIcon name={n} className={`icon__svg ${n}`} />
+                                    </li>
                                 ))}
                             </ul>
                         </div>
