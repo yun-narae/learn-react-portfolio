15b7b2876bf7e01c1aa01919dfb37efe5a1cdb7c
narae <skfo0827@naver.com>
2025-10-29 17:47:49 +0900

    refactor: 로고 구조 정리 및 접근성 속성(aria-label) 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,22 @@
 // src/components/Header.jsx
 import React from "react";
 import { headerNav } from "../constants";
+import SvgIcon from './SvgIcon/SvgIcon';
 
 const Header = () => {
     return (
         <header id="header" role="banner">
             <div className="header__inner">
                 {/* 로고 */}
-                <h1 className="header__logo">
-                    <a href="/" tabIndex={0}>
-                        portfolio
+                <h1>
+                    <a href="/" tabIndex={0} aria-label="홈으로 이동">
+                        <SvgIcon 
+                            className="header__icon"
+                            name="small-flame"
+                        />
                     </a>
                 </h1>
-
+                
                 {/* 데스크톱 전용 네비게이션 (모바일에서는 CSS로 숨김) */}
                 <nav className="header__nav" role="navigation" aria-label="메인 메뉴">
                     <ul className="menu">
